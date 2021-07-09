ed26cf0bc08224d4b0830585903c30d976240fdc
veronica romero <vdrr89@gmail.com>
2021-07-09 00:07:52 -0300

    mobile sizes fixed

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-// var menuProjectsButton = document.getElementById('menu-projects-button');
 var menuProjectsList = document.getElementById('menu-projects-list');
+// var menuProjectButton = document.getElementById('menu-projects-button');
 
-// menuProjectsButton.addEventListener('click', openDropdown());
-// menuProjectsButton.addEventListener('keypress', openDropdown());
+// --- HAMBURGER MENU ---
 
-function openDropdown(){ 
-    if(menuProjectsList.classList.contains("sectionDisabled")){
-        menuProjectsList.remove("sectionDisabled");
+function openHamb(){
+    if(menuProjectsList.style.display === "inline-grid"){
+        menuProjectsList.style.display = "none";
     } else {
-        menuProjectsList.classList.add("sectionDisabled");
+        menuProjectsList.style.display = "inline-grid";
     }
 }
 
+// --- DROPDOWN PROYECTOS MENU ---
+
+
+function openDropdown(){
+    menuProjectsList.classList.toggle("sectionEnabled");
+    console.log("openDropdown simple");
+}
+
+// menuProjectButton.addEventListener("click", openDropdown());
+
+// function openDropdown(){ 
+//     if(menuProjectsList.classList.contains("sectionDisabled")){
+//         menuProjectsList.remove("sectionDisabled");
+//         console.log("openDropdown if")
+//     } else {
+//         menuProjectsList.classList.add("sectionDisabled");
+//         console.log("openDropdown else")
+//     }
+// }
