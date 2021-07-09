632cd03c1a07d3f75f9ab84d678126d5768fe212
veronica romero <vdrr89@gmail.com>
2021-07-09 19:37:23 -0300

    funcionan botones

--- main.js
+++ main.js
@@ -1,32 +1,24 @@
-var menuProjectsList = document.getElementById('menu-projects-list');
+// var menuHamburgerButton = document.getElementById('hamburger-menu-icon');
+var menuList = document.getElementById('menu-list');
 // var menuProjectButton = document.getElementById('menu-projects-button');
+var menuProjectsList = document.getElementById('menu-projects-list');
 
 // --- HAMBURGER MENU ---
 
 function openHamb(){
-    if(menuProjectsList.style.display === "inline-grid"){
-        menuProjectsList.style.display = "none";
+    if(menuList.style.display === "inline-flex"){
+        menuList.style.display = "none";
     } else {
-        menuProjectsList.style.display = "inline-grid";
+        menuList.style.display = "inline-flex";
     }
 }
 
 // --- DROPDOWN PROYECTOS MENU ---
 
-
 function openDropdown(){
-    menuProjectsList.classList.toggle("sectionEnabled");
-    console.log("openDropdown simple");
+    if(menuProjectsList.style.display === "block"){
+        menuProjectsList.style.display = "none";
+    } else {
+        menuProjectsList.style.display = "block";
+    }
 }
-
-// menuProjectButton.addEventListener("click", openDropdown());
-
-// function openDropdown(){ 
-//     if(menuProjectsList.classList.contains("sectionDisabled")){
-//         menuProjectsList.remove("sectionDisabled");
-//         console.log("openDropdown if")
-//     } else {
-//         menuProjectsList.classList.add("sectionDisabled");
-//         console.log("openDropdown else")
-//     }
-// }
